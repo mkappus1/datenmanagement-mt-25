2570be5fec8081e4cca5807eed1b5dc075210539
mkappus1 <98317932+mkappus1@users.noreply.github.com>
2025-11-03 17:20:21 +0100

    Refactor language detection in TMX files

--- Lektion7/MTUOC-TMXdetectlanguagesDIR.py
+++ Lektion7/MTUOC-TMXdetectlanguagesDIR.py
@@ -1,57 +1,28 @@
-#    MTUOC-TMXdetectlanguages
-#    Copyright (C) 2025  Antoni Oliver
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-import argparse
-import xml.etree.ElementTree as ET
-import sys
-import codecs
-import os
-
 def detectlanguagesDIR(direntrada):
-    langs={}
+    langs = {}
     for root, dirs, files in os.walk(direntrada):
         for file in files:
-            try:
-                print(file)
-                if file.endswith(".tmx"):
-                    context = ET.iterparse(file, events=("start", "end"))
-                    root = next(context)  # Get the root element
-                    sl_text = ""
-                    tl_text = ""
-                    langs={}
+            if file.endswith(".tmx"):
+                file_path = os.path.join(root, file)
+                try:
+                    print(file)
+                    context = ET.iterparse(file_path, events=("start", "end"))
+                    root_elem = next(context)  # Get the root element
+                    langs = {}
+
                     for event, elem in context:
                         if event == "end" and elem.tag == "tu":
                             for tuv in elem.findall("tuv"):
-                                try:
-                                    lang = tuv.attrib.get('{http://www.w3.org/XML/1998/namespace}lang')
-                                    langs[lang]=1        
-                                except:
-                                    pass
+                                lang = tuv.attrib.get('{http://www.w3.org/XML/1998/namespace}lang')
+                                if lang:
+                                    langs[lang] = langs.get(lang, 0) + 1
+                    # Ausgabe pro Datei
+                    print(f"Sprachen in {file}: {', '.join(langs.keys())}")
 
-            except:
-                print("ERROR in ",file,sys.exc_info())
+                except Exception as e:
+                    print(f"ERROR in {file_path}: {e}")
                         
+    # Optional: Zusammenfassung aller Sprachen
+    print("\n--- Alle erkannten Sprachcodes ---")
     for l in langs:
         print(l)
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='MTUOC program for detecting the language code of all TMX files in a given directory.')
-    parser.add_argument('-d','--dir', action="store", dest="inputdir", help='The input directory where the TMX files are located.',required=True)
-    args = parser.parse_args()
-    direntrada=args.inputdir
-    detectlanguagesDIR(direntrada)
-
